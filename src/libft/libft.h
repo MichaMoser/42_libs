62943d6154ca3354950318cdb88b54a721d3d151
Michael Moser <Michael.Moser11@web.de>
2024-05-20 10:45:12 +0200

    Add wordlen foo

--- src/libft/libft.h
+++ src/libft/libft.h
@@ -37,6 +37,7 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
+size_t	ft_wordlen(const char *str);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
