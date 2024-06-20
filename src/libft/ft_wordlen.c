ed460e28b1b448a41d69c92bcd8f0fdd57307e56
Michael Moser <Michael.Moser11@web.de>
2024-06-20 15:11:17 +0200

    Fix bug

--- src/libft/ft_wordlen.c
+++ src/libft/ft_wordlen.c
@@ -6,7 +6,7 @@
 /*   By: mmoser <mmoser@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 10:44:21 by mmoser            #+#    #+#             */
-/*   Updated: 2024/05/20 10:44:58 by mmoser           ###   ########.fr       */
+/*   Updated: 2024/06/20 14:23:47 by mmoser           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t	ft_wordlen(const char *str)
 	if (!str)
 		return (0);
 	i = 0;
-	while (str[i] && !is_whitespace(str[i]))
+	while (str[i] && !ft_is_whitespace(str[i]))
 	{
 		i++;
 	}
